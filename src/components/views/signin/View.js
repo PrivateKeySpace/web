2a0513a981f421f1e900d9e2da6c74654340ca4b
Danyil Karuna <kd@oked.me>
2018-04-13 18:35:13 +0300

    PKSP-29 Signin page WIP

--- src/components/views/signin/View.js
+++ src/components/views/signin/View.js
@@ -2,7 +2,10 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { compose, setDisplayName, pure, withHandlers } from 'recompose'
 import { css } from 'aphrodite/no-important'
+import { Footer } from '../../layouts/WalletLayout/components'
+import startLogo from '../../../assets/start.png'
 import tx from '../../../theme/styles'
+import sx from './styles'
 import { mapDispatch, handlers } from './lib'
 
 const displayName = 'components/views/signin/View'
@@ -18,9 +21,32 @@ function View (props) {
   const { handleSignIn } = props
   return (
     <div className={css(tx.p0)}>
-      <button type='button' onClick={handleSignIn}>
-        Log in with Trezor
-      </button>
+      <div className={css(tx.flex, tx.flexDirectionColumn, tx.alignItemsCenter, tx.justifyContentCenter, sx.header)}>
+        <h1 className={css(tx.fontWeight100, tx.textTransformUppercase, tx.mb4, sx.title)}>Private Key Space Wallet</h1>
+        <h2 className={css(tx.fontWeight100, tx.textTransformUppercase, tx.textAlignCenter, sx.subtitle)}>PKS Wallet is an easy-to-use multi-signature wallet for your Trezor device.</h2>
+        <div className={css(sx.separator)} />
+        <div className={css(tx.flex, tx.flexDirectionRow, tx.textTransformUppercase, sx.getTrezor)}>
+          Don't have a Trezor?&nbsp;<a className={css(sx.getTrezorLink)} to='http://trezor.com'>Get here</a>
+        </div>
+        <div
+          className={css(tx.flex, tx.flexDirectionRow, tx.alignItemsCenter, tx.p3, sx.connectTrezor)}
+          onClick={handleSignIn}
+        >
+          <div className={css(tx.flex, tx.justifyContentFlexEnd, tx.alignItemsFlexEnd, tx.flexShrink0, tx.flexGrow0, sx.trezorLogo)}>
+            <img className={css(tx.mb2, tx.mr2)} src={startLogo} alt='Sign in with Trezor' />
+          </div>
+          <div className={css(tx.flexGrow1, tx.textTransformUppercase, tx.textAlignCenter, sx.trezorMessage)}>
+            <div className={css(sx.mainMessage)}>Sign In</div>
+            <div className={css(sx.subMessage)}>with Trezor</div>
+          </div>
+        </div>
+      </div>
+      <div className={css(tx.textAlignCenter, tx.textTransformUppercase, sx.insertHint)}>
+          Insert your device to the computer.
+        <br />
+          Trezor is required to log in to the application.
+      </div>
+      <Footer />
     </div>
   )
 }
---
+++ src/components/views/signin/styles.js
@@ -0,0 +1,73 @@
+import { StyleSheet } from 'aphrodite/no-important'
+import { darken } from 'polished'
+import { colors } from '../../../theme/constants'
+
+const styles = StyleSheet.create({
+  header: {
+    position: 'relative',
+    backgroundColor: colors.darkBg,
+    color: colors.white,
+    height: '50vh',
+    minHeight: 400
+  },
+  title: {
+    fontSize: 38
+  },
+  subtitle: {
+    fontSize: 14,
+    width: 400
+  },
+  separator: {
+    height: 1,
+    width: 86,
+    marginTop: 40,
+    marginBottom: 40,
+    backgroundColor: colors.gray
+  },
+  getTrezor: {
+    fontSize: 14,
+    marginBottom: 75
+  },
+  getTrezorLink: {
+    fontSize: 14,
+    lineHeight: 'normal',
+    color: colors.info,
+    cursor: 'pointer',
+    ':hover': {
+      color: darken(0.1, colors.info),
+      textDecoration: 'underline'
+    }
+  },
+  connectTrezor: {
+    position: 'absolute',
+    bottom: 0,
+    left: '50%',
+    transform: 'translate(-50%, 50%)',
+    width: 400,
+    height: 115,
+    backgroundColor: colors.success,
+    borderRadius: 115,
+    cursor: 'pointer',
+    userSelect: 'none'
+  },
+  trezorLogo: {
+    width: 85,
+    height: 85,
+    borderRadius: '50%'
+  },
+  trezorMessage: {
+    paddingRight: 50
+  },
+  mainMessage: {
+    fontSize: 35
+  },
+  subMessage: {
+    fontSize: 24
+  },
+  insertHint: {
+    marginTop: 80,
+    color: colors.gray
+  }
+})
+
+export default styles
